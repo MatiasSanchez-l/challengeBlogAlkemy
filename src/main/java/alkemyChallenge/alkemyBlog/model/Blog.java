52887bcdf5c21a0f5dce5b840bb3b8ece3455c65
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-20 09:58:33 -0300

    se agrego atributos a blog

--- src/main/java/alkemyChallenge/alkemyBlog/model/Blog.java
+++ src/main/java/alkemyChallenge/alkemyBlog/model/Blog.java
@@ -1,4 +1,75 @@
 package alkemyChallenge.alkemyBlog.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Blogs")
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "contenido")
+    private String contenido;
+
+    @Column(name = "imagen")
+    private String imagen;
+
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column(name = "fecha")
+    private Date fecha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
