36a1e9e15e8dfe6def4429a8edaebf4bea5f2645
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-20 10:13:38 -0300

    se creo blog service interface

---
+++ src/main/java/alkemyChallenge/alkemyBlog/service/BlogService.java
@@ -0,0 +1,10 @@
+package alkemyChallenge.alkemyBlog.service;
+
+import alkemyChallenge.alkemyBlog.model.Blog;
+
+import java.util.List;
+
+public interface BlogService {
+    List<Blog> getAllBlogs();
+    Blog getBlogById(Long id);
+}
