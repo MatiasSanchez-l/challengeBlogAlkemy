03cd5c97123d0a4953e5601a0459df24336c3ae6
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-20 10:09:07 -0300

    se creo blog repository

---
+++ src/main/java/alkemyChallenge/alkemyBlog/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package alkemyChallenge.alkemyBlog.repository;
+
+import alkemyChallenge.alkemyBlog.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+}
