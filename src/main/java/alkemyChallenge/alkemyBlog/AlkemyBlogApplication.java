e45e31f0d63ff9326c12479f2d8c25be75f06c5f
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-20 09:12:11 -0300

    primer commit

---
+++ src/main/java/alkemyChallenge/alkemyBlog/AlkemyBlogApplication.java
@@ -0,0 +1,13 @@
+package alkemyChallenge.alkemyBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alkemyChallenge/alkemyBlog/AlkemyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package alkemyChallenge.alkemyBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
