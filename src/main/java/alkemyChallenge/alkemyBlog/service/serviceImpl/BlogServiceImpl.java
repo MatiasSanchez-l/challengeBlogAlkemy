7373f766137c261eed1e7605907786808c5690bb
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-20 10:28:50 -0300

    Se creo metodo getAllBlogs

---
+++ src/main/java/alkemyChallenge/alkemyBlog/service/serviceImpl/BlogServiceImpl.java
@@ -0,0 +1,24 @@
+package alkemyChallenge.alkemyBlog.service.serviceImpl;
+
+import alkemyChallenge.alkemyBlog.model.Blog;
+import alkemyChallenge.alkemyBlog.repository.BlogRepository;
+import alkemyChallenge.alkemyBlog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class BlogServiceImpl implements BlogService {
+     @Autowired
+     private BlogRepository blogRepository;
+
+    @Override
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll(Sort.by(Sort.Direction.ASC, "fecha"));
+    }
+
+    @Override
+    public Blog getBlogById(Long id) {
+        return null;
+    }
+}
