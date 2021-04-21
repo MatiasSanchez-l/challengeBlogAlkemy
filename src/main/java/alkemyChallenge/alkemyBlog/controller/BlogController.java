f98ed055f13de7597bb22cdc08f2b8c1939c4b1a
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-21 09:51:01 -0300

    requestmapping added to blogcontroller

--- src/main/java/alkemyChallenge/alkemyBlog/controller/BlogController.java
+++ src/main/java/alkemyChallenge/alkemyBlog/controller/BlogController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/posts")
 public class BlogController {
     @Autowired
     private BlogService blogService;
