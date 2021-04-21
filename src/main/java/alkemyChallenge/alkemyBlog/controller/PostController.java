6d9225dbccb5c66c7ceeba83b910f5904731d5af
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-21 15:14:32 -0300

    savePost method added

--- src/main/java/alkemyChallenge/alkemyBlog/controller/PostController.java
+++ src/main/java/alkemyChallenge/alkemyBlog/controller/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,5 +31,11 @@ public class PostController {
         model.addAttribute("post", post);
         model.addAttribute("title", "CREAR POST");
         return "new_post";
+    }
+
+    @PostMapping("/savePost")
+    public String savePost(@ModelAttribute("post") Post post){
+        postService.savePost(post);
+        return "redirect:/posts/";
     }
 }
