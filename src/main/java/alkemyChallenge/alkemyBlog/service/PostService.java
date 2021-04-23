8fcfadc2f0ef4684ab6e978ac6bcd8e48815425f
Sanchez Matias <lomatiasleandro@gmail.com>
2021-04-23 09:53:56 -0300

    findPaginated method added

--- src/main/java/alkemyChallenge/alkemyBlog/service/PostService.java
+++ src/main/java/alkemyChallenge/alkemyBlog/service/PostService.java
@@ -1,6 +1,7 @@
 package alkemyChallenge.alkemyBlog.service;
 
 import alkemyChallenge.alkemyBlog.model.Post;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -9,4 +10,5 @@ public interface PostService {
     Post getPostById(Long id);
     void savePost(Post post);
     void deletePostById(Long id);
+    Page<Post> findPaginated(int pageNro, int pageSize);
 }
